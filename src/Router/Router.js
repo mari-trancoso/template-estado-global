af025c5e97b2afdd7360ad421ed53235d967b148
mari-trancoso <mnegraotrancoso@gmail.com>
2022-11-30 15:36:12 -0300

    exercicio manhã

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mercadinho from "../Pages/Mercadinho";
 import Carrinho from "../Pages/Carrinho";
-export default function Router() {
-    
+import Cadastro from "../Pages/Cadastro";
+import frutaria from "../frutaria.json";
 
+
+export default function Router() {
+    const [frutas, setFrutas] = useState(frutaria.frutaria);
+    const [carrinho, setCarrinho] = useState([])
 
     return (
         <BrowserRouter>
             <Routes>
                 <Route
                     path="/"
-                    element={<Mercadinho/>}
+                    element={<Mercadinho frutas={frutas} setFrutas={setFrutas} carrinho={carrinho} setCarrinho={setCarrinho}/>}
                 />
                 <Route
                     path="/cart"
-                    element={<Carrinho/>}
+                    element={<Carrinho carrinho={carrinho} setCarrinho={setCarrinho}/>}
+                />
+                <Route
+                    path="/cadastro"
+                    element={<Cadastro frutas={frutas} setFrutas={setFrutas}/>}
                 />
 
 
